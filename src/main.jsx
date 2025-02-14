8857cf9ccb908da37a16f8d82d1560954327098d
ZenekSolufka <zenekch22@wp.pl>
2025-02-14 13:52:11 +0100

    switch to supabase for google OAuth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import { createClient } from '@supabase/supabase-js'
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+const supabase = createClient(
+  "https://hvrzpfdknvnjbqmjiqzo.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2cnpwZmRrbnZuamJxbWppcXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1MjQ3MDYsImV4cCI6MjA1NTEwMDcwNn0.7weBhF8Wrev_b4ArJRwN5-8ZvFw9PDUpmHs4qSGkMow"
 )
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SessionContextProvider supabaseClient={supabase}>
+      <App />
+    </SessionContextProvider>
+  </React.StrictMode>
+);
